52b316146a519fdb53127ec8c7c5e7451ef0ca01
Chuz047 <dvegamarvez@proton.me>
2025-02-01 14:38:24 -0600

    Plantilla Inicial.

---
+++ CapaTours/CapaTours/Controllers/AutenticacionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CapaTours.Controllers
+{
+    public class AutenticacionController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
